09e607f10a906c35006e1c86d73335847d99a4e7
maciej.lizewski <maciej.lizewski@gmail.com>
2011-07-18 14:48:22 +0000

    __call handler for webdriver methods in testcase

--- trunk/phpwebdriver/CWebDriverTestCase.php
+++ trunk/phpwebdriver/CWebDriverTestCase.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
   Copyright 2011 3e software house & interactive agency
 
@@ -15,139 +16,136 @@
   limitations under the License.
  */
 
-
 /**
-*  Base class for functional tests using webdriver. 
-*  It provides interface like classic selenium test class.  
-*  @author kolec
-*/
+ *  Base class for functional tests using webdriver.
+ *  It provides interface like classic selenium test class.
+ *  @author kolec
+ */
 class CWebDriverTestCase extends PHPUnit_Framework_TestCase {
+
     public $webdriver;
-    public $baseUrl;	
-
-    function __construct() {
-        parent::__construct();
-    }
-
-    protected function setUp($host="localhost", $port="4444", $browser="firefox")
-    {
-    	parent::setUp();
-        $this->webdriver = new WebDriver($host, $port);
-        $this->webdriver->connect($browser);         
-    }
-
-    protected function tearDown()
-    {
-    	$this->webdriver->close();
-    }
-
-   public function setBrowserUrl($url) {
-	$this->baseUrl = $url;
-   }
-
-    public function open( $url, $check_id = '') {
-        $urlToOpen = $this->baseUrl.$url;
-	
-        $this->webdriver->get($urlToOpen);
-        if( !empty( $check_id ) ) return $this->getElement( LocatorStrategy::id, $check_id ); else usleep(500*1000);
-   }   
-
-   public function getBodyText() {
-        $html = $this->webdriver->getPageSource();
-	$body = preg_replace("/.*<body[^>]*>|<\/body>.*/si", "", $html);
-	return $body;
-   }
-
-   public function isTextPresent($text) {	
-	$found = false;
-        $i = 0;
-        do {
-		$html = $this->webdriver->getPageSource();
-		if (is_string($html)) {
-  			$found = !(strpos($html, $text) === false);
-		}
-		if (!$found) {
-	                sleep( $this->waiting_time );
-	                $i += $this->waiting_time;
-		}
-        } while(!$found && $i <= $this->max_waiting_time );
-	return $found;
-   }
-
-   public function getAttribute($xpath) {
-	$body = $this->getBodyText();
-	$xml = new SimpleXMLElement($body);
-	$nodes = $xml->xpath("$xpath");
-	return $nodes[0][0];
-   }
-   
-
-   public function type( $element_name, $textToType ) {
-	$element = $this->getElement( LocatorStrategy::id, $element_name);
-	if (isset($element)) {
-		///usleep(100*1000);
-		$element->sendKeys(array($textToType));
-		//usleep(500*1000);
-	}	
-   }
-
-   public function clear( $element_name ) {
-	$element = $this->getElement( LocatorStrategy::id, $element_name);
-	if ($element) {
-		$element->clear();
-	}
-   }
-
-   public function submit( $element_name ) {
-	$element = $this->getElement( LocatorStrategy::id, $element_name);
-	if (isset($element)) {
-		$element->submit();
-		usleep(500*1000);
-	}
-   }
-
-
-   public function click( $element_name ) {
-	$element = $this->getElement( LocatorStrategy::id, $element_name);
-	if (isset($element)) {
-		$element->click();
-		usleep(500*1000);
-	}
-   }
-
-   public function close() {
-	$this->webdriver->close();
-   }
-   
-   public function select($element_name, $option_text) {
-        $element = $this->getElement( LocatorStrategy::id, $element_name );
-	$option = $element->findOptionElementByText($option_text);
-	$option->click();
-	//$element->sendKeys(array($option_text));
-        /*$options = $element->findElementsBy(LocatorStrategy::tagName, "option");
-        foreach($options as $option) {
-            if ($option->getText()==$option_text) {
-                $option->setSelected();
-                break;
-            }
-        }*/
-   }
-
-    public function getElementByIdOrName( $element_name ) {
-        try {
-            $element = $this->webdriver->findElementBy(LocatorStrategy::id, $element_name);
-        } catch ( NoSuchElementException $ex) {
-            $element = $this->webdriver->findElementBy(LocatorStrategy::name, $element_name);
-        } 
-        return $element;
-   }
-
-
+    public $baseUrl;
     protected $waiting_time = 0.5;
     protected $max_waiting_time = 4;
 
-    public function getElement( $strategy, $name )
-    {
+    public function __call( $name, $arguments ) {
+        if( method_exists( $this->webdriver, $name ) ) {
+            return call_user_func_array( array( $this->webdriver, $name ), $arguments );
+        }
+    }
+
+    protected function setUp( $host="localhost", $port="4444", $browser="firefox" ) {
+        parent::setUp();
+        $this->webdriver = new WebDriver( $host, $port );
+        $this->webdriver->connect( $browser );
+    }
+
+    protected function tearDown() {
+        $this->webdriver->close();
+    }
+
+    public function setBrowserUrl( $url ) {
+        $this->baseUrl = $url;
+    }
+
+    public function open( $url, $check_id = '' ) {
+        $urlToOpen = $this->baseUrl . $url;
+
+        $this->webdriver->get( $urlToOpen );
+        if( !empty( $check_id ) )
+            return $this->getElement( LocatorStrategy::id, $check_id ); else
+            usleep( 500 * 1000 );
+    }
+
+    public function getBodyText() {
+        $html = $this->webdriver->getPageSource();
+        $body = preg_replace( "/.*<body[^>]*>|<\/body>.*/si", "", $html );
+        return $body;
+    }
+
+    public function isTextPresent( $text ) {
+        $found = false;
+        $i = 0;
+        do {
+            $html = $this->webdriver->getPageSource();
+            if( is_string( $html ) ) {
+                $found = !(strpos( $html, $text ) === false);
+            }
+            if( !$found ) {
+                sleep( $this->waiting_time );
+                $i += $this->waiting_time;
+            }
+        } while( !$found && $i <= $this->max_waiting_time );
+        return $found;
+    }
+
+    public function getAttribute( $xpath ) {
+        $body = $this->getBodyText();
+        $xml = new SimpleXMLElement( $body );
+        $nodes = $xml->xpath( "$xpath" );
+        return $nodes[0][0];
+    }
+
+    public function type( $element_name, $textToType ) {
+        $element = $this->getElement( LocatorStrategy::id, $element_name );
+        if( isset( $element ) ) {
+            ///usleep(100*1000);
+            $element->sendKeys( array( $textToType ) );
+            //usleep(500*1000);
+        }
+    }
+
+    public function clear( $element_name ) {
+        $element = $this->getElement( LocatorStrategy::id, $element_name );
+        if( $element ) {
+            $element->clear();
+        }
+    }
+
+    public function submit( $element_name ) {
+        $element = $this->getElement( LocatorStrategy::id, $element_name );
+        if( isset( $element ) ) {
+            $element->submit();
+            usleep( 500 * 1000 );
+        }
+    }
+
+    public function click( $element_name ) {
+        $element = $this->getElement( LocatorStrategy::id, $element_name );
+        if( isset( $element ) ) {
+            $element->click();
+            usleep( 500 * 1000 );
+        }
+    }
+
+    public function close() {
+        $this->webdriver->close();
+    }
+
+    public function select( $element_name, $option_text ) {
+        $element = $this->getElement( LocatorStrategy::id, $element_name );
+        $option = $element->findOptionElementByText( $option_text );
+        $option->click();
+        //$element->sendKeys(array($option_text));
+        /* $options = $element->findElementsBy(LocatorStrategy::tagName, "option");
+          foreach($options as $option) {
+          if ($option->getText()==$option_text) {
+          $option->setSelected();
+          break;
+          }
+          } */
+    }
+
+    public function getElementByIdOrName( $element_name ) {
+        try {
+            $element = $this->webdriver->findElementBy( LocatorStrategy::id, $element_name );
+        } catch( NoSuchElementException $ex ) {
+            $element = $this->webdriver->findElementBy( LocatorStrategy::name, $element_name );
+        }
+        return $element;
+    }
+
+    public function getElement( $strategy, $name ) {
         $i = 0;
         do {
             try {
@@ -157,12 +155,12 @@ class CWebDriverTestCase extends PHPUnit_Framework_TestCase {
                 sleep( $this->waiting_time );
                 $i += $this->waiting_time;
             }
-        } while(!isset($element) && $i <= $this->max_waiting_time );
-        if( !isset($element) ) $this->fail( "Element has not appeared after " . $this->max_waiting_time . " seconds." );
+        } while( !isset( $element ) && $i <= $this->max_waiting_time );
+        if( !isset( $element ) )
+            $this->fail( "Element has not appeared after " . $this->max_waiting_time . " seconds." );
         return $element;
-   }
+    }
+
 }
-
-
 
 ?>
